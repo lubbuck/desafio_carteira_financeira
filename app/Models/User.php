2c7fe2f6e4d8951c6ce06373c739f3a11578f9a8
lubbuck <antonioluisp97@gmail.com>
2025-07-13 16:03:49 -0300

    fix: user com uuid e user no seed

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $keyType = 'string';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->primaryKey} = Uuid::uuid4();
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +57,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isLogged()
+    {
+        return auth()->id() == $this->id;
     }
 }
